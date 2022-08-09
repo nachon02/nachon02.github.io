38369fef8566e4ec1f383c0ff54713ae3bc5710b
nachon02 <110431737+nachon02@users.noreply.github.com>
2022-08-09 20:13:28 -0300

    agrego boton hamburguesa y cambio paleta de colores

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-const boton1 = document.getElementById("button1");
+const menu = document.querySelector(".bars__menu");
+menu.addEventListener("click", animarBarras);
+let line1__bars = document.querySelector(".line1__bars-menu");
+let line2__bars = document.querySelector(".line2__bars-menu");
+let line3__bars = document.querySelector(".line3__bars-menu");
+const nav = document.querySelector(".nav-container");
 
-boton1.addEventListener("click", function (e) {
-    const valor = document.getElementById("input1").value;
-    const titulo = document.querySelector(".titulo");
-    titulo.innerHTML = valor;
-});
+function animarBarras() {
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+
+    nav.classList.toggle("nav-container__active");
+}
